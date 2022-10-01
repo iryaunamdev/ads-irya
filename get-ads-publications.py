885a75bbb0777ed5a8de16728876bda755316044
Leo Arroyo <laonrdo@gmail.com>
2022-09-30 19:56:53 -0500

    initial commit

---
+++ get-ads-publications.py
@@ -0,0 +1,212 @@
+from random import choices
+import sys
+import ads
+import json
+import re
+import html
+import csv
+from datetime import datetime
+from fuzzywuzzy import process
+from unidecode import unidecode
+import pandas as pd
+
+
+# Fields to retrive from ADS query
+fields = [
+    "bibcode",
+    "title",
+    "author",
+    "aff",
+    "year",
+    "pub",
+    "volume",
+    "page",
+    "pubdate",
+    "date",
+    "bibstem",
+    "citation",
+    "citation_count",
+    "doctype",
+    "orcid",
+    "property"
+    
+]
+
+papersByAuthor = []
+papersByOrcid =[]
+papersAll =[]
+
+def fuzzy(s, squeeze=True):
+    """Transform string for a fuzzy comparison
+
+    1. Decode HTML entities: "&amp;" -> "&"
+    2. Eliminate accents: "Astrofísica" -> "Astrofisica"
+    3. Fold case: "Astrofísica" -> "astrofisica"
+    4. If optional argument `squeeze` is True, then remove
+       non-word characters: "onom&ia y astro" -> "onomiayastro"
+    """
+    rslt = unidecode(html.unescape(s)).casefold()
+    if squeeze:
+        rslt = re.sub(r"\W", "", rslt)
+    return rslt
+
+def query_author(author:str)->list:
+    papers = list(
+        ads.SearchQuery(
+            q=f"author:{author}",
+            fl=fields,
+            rows=2000,
+            sort="date+desc",
+        )
+    )
+    return papers
+
+def makeRow(data):
+    pass
+
+def query_orcid(orcid:str)->list:
+    papers = list(
+        ads.SearchQuery(
+            q=f"orcid:{orcid}",
+            fl=fields,
+            rows=2000,
+            sort="date+desc",
+        )
+    )
+    return papers
+
+def unificatedPapers(papersListA, papersListB):
+    unificated = []
+    for paperA in papersListA:
+        for paperB in papersListB:
+            match_paper: False
+            if paperA.bibcode == paperB.bibcode:
+                match_paper = True
+                break
+        if match_paper:
+            unificated.append(paperB)
+        else:
+            unificated.append(paperB)
+            unificated.append(paperA)
+        
+    return unificated
+
+def papers2CSV(dataArray, author_id):
+    data = []
+    for paper in dataArray:
+        values = [
+                paper.id,
+                paper.bibcode,
+                paper.title,
+                paper.author,
+                paper.aff,
+                paper.year,
+                paper.pub,
+                paper.volume,
+                paper.page,
+                paper.pubdate,
+                paper.date,
+                paper.bibstem,
+                paper.citation,
+                paper.citation_count,
+                paper.doctype,
+                paper.orcid,
+                paper.property
+            ]
+        choices = paper.author
+        author_match_score = process.extractOne(fuzzy(author[2], squeeze=False), choices)
+        values.append(author_match_score)
+                
+        choices = paper.orcid
+        orcid_match_score = process.extractOne(author[1], choices)
+        values.append(orcid_match_score)
+        
+        data.append(values)
+        
+    fields.append('author_match')
+    fields.append('orcid_match')
+        
+    with open(author_id+'.tsv', 'w', encoding='UTF8') as file:
+        dw = csv.writer(file, delimiter='\t')
+        dw.writerow(fields)
+        dw.writerows(data)
+    
+    return True
+    
+def papers2JSON(dataArray, author_id):
+    papersJSON = {}
+    for paper in dataArray:
+        values = [
+                paper.id,
+                paper.bibcode,
+                paper.title,
+                paper.author,
+                paper.aff,
+                paper.year,
+                paper.pub,
+                paper.volume,
+                paper.page,
+                paper.pubdate,
+                paper.date,
+                paper.bibstem,
+                paper.citation,
+                paper.citation_count,
+                paper.doctype,
+                paper.orcid,
+                paper.property
+            ]
+        dict_row = dict(zip(fields, values))
+        
+        choices = paper.author
+        author_match_score = process.extractOne(fuzzy(author[2], squeeze=False), choices)
+        dict_row['match_author'] = author_match_score
+        
+        choices = paper.orcid
+        orcid_match_score = process.extractOne(author[1], choices)
+        dict_row['match_orcid'] = orcid_match_score
+        
+        papersJSON[paper.bibcode] = dict_row
+      
+    with open(author_id+".json", "w", encoding='utf-8') as outfile:
+        json.dump(papersJSON, outfile, ensure_ascii=False)
+    
+    return True
+        
+
+if __name__=="__main__":
+    try:
+        AUTHOR_LIST_FILE = sys.argv[1]
+    except IndexError:
+        sys.exit(f"Usage: {sys.argv[0]} AUTHOR [Y_START] [Y_END]")
+        
+    try:
+        Y_START = sys.argv[2]
+    except IndexError:
+        Y_START = datetime.today().year
+        
+    try:
+        Y_START = sys.argv[3]
+    except IndexError:
+        Y_START = datetime.today().year
+        
+    author_list_file = open(AUTHOR_LIST_FILE, 'r')
+    author_list = author_list_file.read().split('\n')
+    for author in author_list:
+        
+        print(author, end=" ")
+        author = author.split('|')
+        
+        #Get papers search
+        papersByAuthor= query_author(fuzzy(author[2], squeeze=False))
+        print("papersByAuthor: ", len(papersByAuthor), end=" ")
+        if author[1]:
+            papersByOrcid =query_orcid(author[1])
+            print("papersByOrcid: ", len(papersByOrcid), end=" ")
+            papersAll = unificatedPapers(papersByAuthor, papersByOrcid)
+        else:
+            papersAll = papersByAuthor          
+        print("papersAll: ", len(papersAll), end=" ")
+        
+        papers2JSON(papersAll, author[0])
+        papers2CSV(papersAll, author[0])
+        print()
---
+++ make-irya-publications-list.py
@@ -0,0 +1,340 @@
+"""
+make_irya_publication_list.py
+
+Creates the web page publication_list2.php, which contains lists of
+refereed journal publications by members of the IRyA, organized by
+year from 2003 to the present.  Also creates the web page
+latest_publication2.php with the most recent paper.
+
+Usage:
+
+    make_irya_publication_list.py OUTPUT_FOLDER
+
+Files will be written to OUTPUT_FOLDER
+
+See accompanying file README.md for further options.
+
+Authors: Will Henney and Jane Arthur, 2022
+"""
+
+# Standard library imports
+import sys
+import json
+import datetime
+import re
+from textwrap import dedent
+import html
+from typing import Tuple  # required for earlier python versions
+
+# Third-party library
+# Install with "pip install ads"
+# Requires API key, see step 2 of "Getting Started" at https://ads.readthedocs.io/en/latest/
+import ads
+from unidecode import unidecode
+
+PUB_LIST_FILE = "publication_list2.php"
+LATEST_PUB_FILE = "latest_publication2.php"
+DIAGNOSTIC_FILE = "nonstandard_variants.txt"
+
+
+# Add bibcodes to this list in order to inspect for debugging purposes
+DEBUG_BIBCODES = [
+    # "2020ApJ...905...25G",  # Failed highlighting in author list
+    # "2007JKAS...40..137K",  # Stan paper with "&" in affiliation
+]
+
+# We look for these different variants of the institute name
+#
+# Any special cases due to journal errors should be added to the end
+# of this list
+irya_variants = [
+    "CRyA",
+    "IRyA",
+    "Radioastronomía y Astrofísica",
+    "Radiostronomía y Astrofísica",  # Ricardo Gonzalez paper
+    "Radioastronomí a y Astrofí sica",  # For Gustavo Bruzual 2021 paper
+    "Radioastronomía y Astrofísca",  # Stan paper
+    "Radioastronomía y Astrofsica",  # In anticipapation
+    "Radioastromomía y Astrofísica",  # Just in case
+]
+
+# All except the first 3 variants are mis-spellings, about which we
+# may want to report diagnostics
+nonstandard_variants = irya_variants[3:]
+
+# List for saving papers that we find with nonstandard affiliation
+nonstandard_papers = []
+
+affstring = "(" + " OR ".join([f'"{_}"' for _ in irya_variants]) + ")"
+
+# To eliminate false positives in Italy, we have an auxiliary check on UNAM or Morelia
+unam_variants = [
+    '"UNAM"',
+    '"Mexico"',
+    '"Morelia"',
+]
+affstring2 = "(" + " OR ".join(unam_variants) + ")"
+
+
+# These are the fields that we want the ADS query to give us
+fields = [
+    "id",
+    "bibcode",
+    "title",
+    "author",
+    "aff",
+    "year",
+    "pub",
+    "volume",
+    "page",
+    "pubdate",
+]
+
+
+def fuzzy(s, squeeze=True):
+    """Transform string for a fuzzy comparison
+
+    1. Decode HTML entities: "&amp;" -> "&"
+    2. Eliminate accents: "Astrofísica" -> "Astrofisica"
+    3. Fold case: "Astrofísica" -> "astrofisica"
+    4. If optional argument `squeeze` is True, then remove
+       non-word characters: "onom&ia y astro" -> "onomiayastro"
+    """
+    rslt = unidecode(html.unescape(s)).casefold()
+    if squeeze:
+        rslt = re.sub(r"\W", "", rslt)
+    return rslt
+
+
+def mark_irya_affiliations(paper):
+    """Highlight authors with IRyA affiliation
+
+    This mutates the paper.author list, so should be called only once
+    """
+    for i, [author, affil] in enumerate(zip(paper.author, paper.aff)):
+        for variant in irya_variants:
+            if fuzzy(variant) in fuzzy(affil):
+                paper.author[i] = f"<strong>{author}</strong>"
+                break
+
+
+def check_nonstandard_affiliations(paper):
+    """Check for common mispellings with IRyA affiliation"""
+    for i, [author, affil] in enumerate(zip(paper.author, paper.aff)):
+        for variant in nonstandard_variants:
+            if fuzzy(variant, squeeze=False) in fuzzy(affil, squeeze=False):
+                nonstandard_papers.append([paper.bibcode, i, author, affil])
+                break
+
+
+def make_author_list(paper, nmax=20):
+    """Format list of authors but hide full list if very long
+
+    Adds javascript toggle button to show full list
+    """
+    if len(paper.author) <= nmax:
+        return "; ".join(paper.author)
+    else:
+        rslt = "; ".join(paper.author[:nmax])
+        rslt += f'; <span id="{paper.bibcode}.authors" style="display: none;">'
+        rslt += "; ".join(paper.author[nmax:])
+        rslt += dedent(
+            f"""\
+            </span><span id="{paper.bibcode}.button"></span>
+            <script>
+            toggleAuthors('{paper.bibcode}', {len(paper.author)}, 0);
+            </script>
+            """
+        )
+        return rslt
+
+
+def format_paper(paper):
+    """Make an html list item for a single paper
+
+    Paper title is link to ADS page, followed by author list, date, journal, volume, page
+    """
+    bibcode_link = dedent(
+        f"""\
+        <a href="https://ui.adsabs.harvard.edu/abs/{paper.bibcode}" target="_blank">
+        <i>{paper.title[0]}</i>
+        </a>
+        """
+    )
+    author_list = make_author_list(paper)
+    year_month = "/".join(paper.pubdate.split("-")[:-1])
+    rslt = "<li><p>\n"
+    rslt += bibcode_link
+    rslt += "<br/>\n" + author_list + "\n<br/>\n"
+    rslt += ", ".join([year_month, paper.pub, paper.volume, paper.page[0]]) + "\n"
+    rslt += "</p></li>\n"
+    return rslt
+
+
+# This javascript header goes at top of the files.  We believe it was
+# written by Vicente Rodríguez Gómez
+script_header = """\
+<script>
+function toggleAuthors(bibcode, numAuthors, longList) {
+  var x = document.getElementById(bibcode + '.authors');
+  var y = document.getElementById(bibcode + '.button');
+
+  var showLess = ' <a href="javascript:void(0)" onclick="toggleAuthors(' + "'" + bibcode + "'" + ', ' + numAuthors + ', 0)" style="margin-left: 6px;"><em>show less</em></a>';
+  var showMore = ' <a href="javascript:void(0)" onclick="toggleAuthors(' + "'" + bibcode + "'" + ', ' + numAuthors + ', 1)" style="margin-left: 6px;"><em>show ' + (numAuthors-20) + ' more</em></a>';
+
+  if (longList === 0) {
+    x.style.display = 'none';
+    y.innerHTML = showMore;
+  } else {
+    x.style.display = 'inline';
+    y.innerHTML = showLess;
+  }
+};
+</script>
+"""
+
+
+def query_years(years: list) -> Tuple[str, str]:
+    """Execute all the queries and construct the pages"""
+
+    pub_list_page = script_header
+    latest_pub_page = script_header
+
+    # Start outer div
+    pub_list_page += """\
+    <div class="row-fluid">
+    """
+
+    # Make navigation sidebar with links for each year
+    pub_list_page += """\
+    <div id="sidebar" class="span1">
+    <ul class="nav nav-pills nav-stacked mod-list">
+    """
+
+    for year in years:
+        pub_list_page += (
+            f'<li style="width: 58px;"><a href="#{year}" data-toggle="pill">{year}</a></li>'
+            + "\n"
+        )
+
+    # End navigation sidebar
+    pub_list_page += """\
+    </ul>
+    </div>
+    """
+
+    # Start div with the main content
+    pub_list_page += """\
+    <div class="tab-content span11">
+    """
+
+    for year in years:
+        tab_pane = "tab-pane active" if year == this_year else "tab-pane"
+        # Make a single ADS query for each year
+        papers = list(
+            ads.SearchQuery(
+                q=f"aff:{affstring}",
+                fq=f"aff:{affstring2} AND property:refereed",
+                fl=fields,
+                year=f"{year}",
+                rows=2000,
+                sort="date+desc",
+            )
+        )
+        # Remove MNRAS early publication papers from list
+        papers = [_ for _ in papers if not ".tmp." in _.bibcode]
+
+        # Start a div for this year with list of papers
+        pub_list_page += dedent(
+            f"""\
+            <div class="{tab_pane}" id="{year}">
+            <h4 style="text-indent: 10px;">Publications {year}</h4>
+            <ol>      
+            """
+        )
+
+        # Add a list item for each paper
+        for paper in papers:
+            check_nonstandard_affiliations(paper)
+            mark_irya_affiliations(paper)
+            pub_list_page += format_paper(paper)
+            if paper.bibcode in DEBUG_BIBCODES:
+                print("*** DEBUG_BIBCODE", paper.bibcode)
+                print(paper.items())
+
+        # Close the list and close the div for this year
+        pub_list_page += dedent(
+            """\
+            </ol>
+            </div>
+            """
+        )
+        # Also add latest publication to a separate page
+        if year == this_year:
+            latest_pub_page += format_paper(papers[0])
+
+    # Close div with the main content
+    pub_list_page += """\
+    </div>
+    """
+
+    # Close outer div
+    pub_list_page += """\
+    </div>
+    """
+
+    return pub_list_page, latest_pub_page
+
+
+def dump_nonstandard():
+    nauthors = len(nonstandard_papers)
+    # Count unique bibcodes
+    npapers = len(set([_[0] for _ in nonstandard_papers]))
+
+    s = dedent(
+        f"""\
+        ############################################################
+        # Non-standard spellings in IRyA affiliations 2003-present
+        #
+        # Total: {nauthors} authors in {npapers} papers
+        ############################################################
+        """
+    )
+    for bibcode, irank, author, affil in nonstandard_papers:
+        s += dedent(
+            f"""
+            {bibcode}
+            Author {irank + 1}: {author}
+            Affil: {affil}
+            """
+        )
+    with open(f"{OUTPUT_FOLDER}/{DIAGNOSTIC_FILE}", "w") as f:
+        f.write(s)
+
+
+if __name__ == "__main__":
+    try:
+        OUTPUT_FOLDER = sys.argv[1]
+    except IndexError:
+        sys.exit(f"Usage: {sys.argv[0]} OUTPUT_FOLDER [variant]")
+
+    # Optionally write a file with the mispellings
+    try:
+        DO_SAVE_VARIANTS = "variant" in str(sys.argv[2])
+    except IndexError:
+        DO_SAVE_VARIANTS = False
+
+    start_year = 2003
+    this_year = datetime.date.today().year
+    years = list(reversed(range(start_year, this_year + 1)))
+    pub_list_page, latest_pub_page = query_years(years)
+
+    # Write out the two files
+    with open(f"{OUTPUT_FOLDER}/{PUB_LIST_FILE}", "w") as f:
+        f.write(pub_list_page)
+    with open(f"{OUTPUT_FOLDER}/{LATEST_PUB_FILE}", "w") as f:
+        f.write(latest_pub_page)
+
+    if DO_SAVE_VARIANTS:
+        dump_nonstandard()
+
